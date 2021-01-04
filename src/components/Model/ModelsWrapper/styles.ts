6e4c0517f41364ce9085310bafbf6ccb99b70d87
dantls <dan_tls@hotmail.com>
2021-01-04 01:18:59 -0400

    Add Scroll

--- src/components/Model/ModelsWrapper/styles.ts
+++ src/components/Model/ModelsWrapper/styles.ts
@@ -14,14 +14,3 @@ export const OverlaysRoot = styled.div`
   bottom: 0;
   left: 0;
 `;
-export const ModelOverlay = styled.div`
-  position: sticky;
-  top: 0;
-  height: 100vh;
-  margin-top: -100vh;
-
-  display:flex;
-  flex-direction: column;
-  align-items: center;
- 
-`;
